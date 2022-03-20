c02d4e440f3a472acdde2e000fcff5c771abb420
shreyassambare <shreyassambare@localhost>
2022-03-20 11:08:32 +0530

    Added "AddFundPage" class

--- src/main/java/com/phpTravelsDemo/Pages/AddFundPage.java
+++ src/main/java/com/phpTravelsDemo/Pages/AddFundPage.java
@@ -1,5 +1,20 @@
 package com.phpTravelsDemo.Pages;
 
-public class AddFundPage {
+import org.openqa.selenium.support.PageFactory;
+
+import com.phpTravelsDemo.BaseTest.BaseTest;
+
+public class AddFundPage extends BaseTest {
+
+	public AddFundPage() {
+
+		PageFactory.initElements(driver, this);
+
+	}
+
+	public String ValidatePageTitle() {
+
+		return driver.getTitle();
+	}
 
 }
